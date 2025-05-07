1a51a78fa1ff7845f4572268b3edc7795ef04220
Belovedbuy <chukwumabeloved@gmail.com>
2025-05-07 16:31:27 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAWdD_bQCUiPFbI4IiHZS7nv5qdnN2SHc0",
+  authDomain: "sample-firebase-ai-app-71886.firebaseapp.com",
+  projectId: "sample-firebase-ai-app-71886",
+  storageBucket: "sample-firebase-ai-app-71886.firebasestorage.app",
+  messagingSenderId: "1035620568777",
+  appId: "1:1035620568777:web:c80e86dbd10fc480c34718"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
